73beacb7cc86da713e46a1fa2222b385a832709b
Tal Ben Adon <talbenadon2@gmail.com>
2024-09-28 05:24:43 +0300

    removed comment

--- downloader/downloader.py
+++ downloader/downloader.py
@@ -3,7 +3,7 @@ import os
 
 video_dir = './video_temp_dir'
 
-def download_video(video_url: str ): # // if video_dir not provided, create a dir within bot directory for the temp videos
+def download_video(video_url: str ):
 
     os.makedirs(video_dir, exist_ok=True)
     video_path = os.path.join(video_dir,'%(title)s.%(ext)s')
