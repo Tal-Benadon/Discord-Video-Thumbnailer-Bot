efb88afe4cc6daf93f2037a5c3788f10974f55cf
Tal Ben Adon <talbenadon2@gmail.com>
2024-09-30 17:03:31 +0300

    twicked the ydl download to be 'extract_info' for the ability to access file title and extension to correctly return the created video path

--- downloader/downloader.py
+++ downloader/downloader.py
@@ -6,11 +6,12 @@ video_dir = './video_temp_dir'
 def download_video(video_url: str ):
 
     os.makedirs(video_dir, exist_ok=True)
-    video_path = os.path.join(video_dir,'%(title)s.%(ext)s')
+    video_path_template = os.path.join(video_dir,'%(title)s.%(ext)s')
   
-    ydl_opt = {'outtmpl': video_path}
+    ydl_opt = {'outtmpl': video_path_template}
 
     with yt_dlp.YoutubeDL(ydl_opt) as ydl:
-        ydl.download([video_url])
+        info = ydl.extract_info(video_url, download=True)
         
+        video_path = os.path.join(video_dir, f"{info['title']}.{info['ext']}")
     return video_path
