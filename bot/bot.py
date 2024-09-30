4b60e8ed777a3ee08d404d82c8451db34b080dcd
Tal Ben Adon <talbenadon2@gmail.com>
2024-09-30 03:59:38 +0300

    set up intents and client events, todo handle received message

--- bot/bot.py
+++ bot/bot.py
@@ -1,1 +1,37 @@
+from typing import Final
 from discord import Intents, Client, Message, File
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TOKEN:Final[str] = os.getenv('DISCORD_TOKEN2')
+
+intents: Intents = Intents.default()
+intents.message_content = True
+client: Client = Client(intents=intents)
+
+
+@client.event
+async def on_ready() -> None:
+    print(f'{client.user} is now running')
+
+@client.event
+async def on_message(message: Message) -> None: 
+    if message.author == client.user: # // if the bot is the one sending the message, do nothing. //
+        return
+
+    username: str = str(message.author)
+    user_message: str = message.content
+    channel: str = str(message.channel)
+    
+    print(f'[{channel}] {username}: "{user_message}"')
+    
+    await handle_received_message(message, user_message)
+    
+    
+    
+    
+    
+
+# def main() -> None:
+#     Client.run(token=TOKEN)
---
+++ main.py
@@ -0,0 +1,7 @@
+
+from bot.bot import Client
+import os
+
+
+
+    
