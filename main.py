4578f55a966e51dc166759cfbc5a2a9c4f629a87
Tal Ben Adon <talbenadon2@gmail.com>
2024-09-30 17:07:28 +0300

    main now holds load env and the client.run function

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 
-from bot.bot import Client
+from bot.bot import client
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN2')
+
+def main() -> None: 
+   client.run(token=TOKEN)
+   
+if __name__ == '__main__':
+    main()
 
 
     
