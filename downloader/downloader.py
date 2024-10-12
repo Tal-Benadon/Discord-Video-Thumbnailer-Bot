d8fa21f7da248322d843c978f7fec330f14890e7
Tal Ben Adon <talbenadon2@gmail.com>
2024-10-13 00:17:14 +0300

    renaming video path to up to 10 notes, TODO: add facebook title handling

--- downloader/downloader.py
+++ downloader/downloader.py
@@ -3,6 +3,12 @@ import os
 
 video_dir = './video_temp_dir'
 
+def turncate_title(title):
+    return title[0:10]    
+
+def handle_wrong_facebook_title(title):
+    return
+
 def download_video(video_url: str ):
 
     os.makedirs(video_dir, exist_ok=True)
@@ -13,10 +19,17 @@ def download_video(video_url: str ):
     with yt_dlp.YoutubeDL(ydl_opt) as ydl:
         info = ydl.extract_info(video_url, download=False)
         if not info.get('type') is None or info.get('type') != "playlist":
+            
             ydl.download([video_url])
             print(f"info {info}")
-            video_title = info.get('title','unkown_title')
+            initial_title = info.get('title','unkown_title')
             video_ext = info.get('ext', 'mp4')
-            video_path = os.path.join(video_dir, f'{video_title}.{video_ext}')
-            return video_path
+            
+            original_video_path = os.path.join(video_dir, f"{initial_title}.{video_ext}")
+            video_title = turncate_title(initial_title)
+            new_video_path = os.path.join(video_dir, f'{video_title}.{video_ext}')
+            if os.path.exists(original_video_path):
+                os.rename(original_video_path, new_video_path)
+            # video_path = os.path.join(video_dir, f'{video_title}.{video_ext}')
+            return new_video_path
    
